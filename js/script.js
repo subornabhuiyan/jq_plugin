ae79496f7478d0444434c2a34f470bb604444424
subornabhuiyan <85570818+subornabhuiyan@users.noreply.github.com>
2021-10-12 15:16:47 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,62 @@
+
+  //counter up
+
+  $('.counter').counterUp({
+      delay: 10,
+      time: 2000
+    });
+    $('.counter').addClass('animated fadeInDownBig');
+    $('.count-div h3').addClass('animated fadeIn');
+
+  //wow js
+
+  var wow = new WOW(
+      {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       0,          // distance to the element when triggering the animation (default is 0)
+        mobile:       true,       // trigger animations on mobile devices (default is true)
+        live:         true,       // act on asynchronously loaded content (default is true)
+        callback:     function(box) {
+          // the callback is fired every time an animation is started
+          // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null // optional scroll container selector, otherwise use window
+      }
+    );
+    wow.init();
+
+    //AOS
+    AOS.init();
+
+    //slick slider
+    $('.my-slick').slick({
+      
+
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: true,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: true,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  
+  //mixitup gallery
+let mixer = mixitup('.gallery');
